3536622cb701d2005f8d050e9cf88740b6f40469
briannice <brian.nys.dev@gmail.com>
2022-07-10 20:24:07 +0200

    added tailwindcss

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from 'next/app'
+import React from 'react'
+
+import '@styles/main.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
